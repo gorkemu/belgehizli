107354eb4b10f2a785012fa2aed4b8896fabb0c8
gorkemu <gorkemunal88@gmail.com>
2026-03-29 14:53:03 +0300

    update smtp

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,10 +1,13 @@
-const { Resend } = require('resend');
-const resend = new Resend(process.env.EMAIL_PASS);
+// backend/utils/mailer.js
 
 const sendPdfEmail = async (to, subject, text, html, pdfBuffer, pdfFilename) => {
     try {
+        const maskedEmail = to.replace(/^(.)(.*)(.@.*)$/, (_, a, b, c) => a + b.replace(/./g, '*') + c);
+        
+        console.log(`Resend HTTP API üzerinden e-posta gönderimi başlatılıyor...`);
+
         const { data, error } = await resend.emails.send({
-            from: process.env.EMAIL_FROM, 
+            from: process.env.EMAIL_FROM,
             to: to,
             subject: subject,
             html: html,
@@ -17,10 +20,11 @@ const sendPdfEmail = async (to, subject, text, html, pdfBuffer, pdfFilename) => 
         });
 
         if (error) throw new Error(error.message);
-        console.log('E-posta başarıyla gönderildi:', data.id);
+        
+        console.log('E-posta başarıyla gönderildi. İşlem ID:', data.id);
         return data;
     } catch (error) {
-        console.error('Gönderim hatası:', error);
+        console.error('E-posta gönderim hatası oluştu.');
         throw error;
     }
 };
