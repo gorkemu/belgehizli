268c3f6f9e00758aa621a18eab212a4cd5ad0850
gorkemu <gorkemunal88@gmail.com>
2026-04-03 10:33:59 +0300

    UX improvements

--- backend/utils/sanitizer.js
+++ backend/utils/sanitizer.js
@@ -1,17 +1,17 @@
-const DOMPurify = require('isomorphic-dompurify');
+const sanitizeHtml = require('sanitize-html');
 
 const sanitizeHtmlForPdf = (dirtyHtml) => {
   if (!dirtyHtml) return '';
 
-  return DOMPurify.sanitize(dirtyHtml, {
-    ALLOWED_TAGS: [
+  return sanitizeHtml(dirtyHtml, {
+    allowedTags: [
       'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'br', 'b', 'i', 'strong', 
       'em', 'u', 'a', 'ul', 'ol', 'li', 'span', 'div', 'hr', 
       'table', 'thead', 'tbody', 'tr', 'th', 'td', 'pre', 'code', 'style'
     ],
-    ALLOWED_ATTR: ['href', 'target', 'class', 'style', 'src', 'alt', 'rel'],
-    FORBID_TAGS: ['script', 'iframe', 'object', 'embed', 'form', 'link'],
-    FORBID_ATTR: ['onerror', 'onload', 'onclick', 'onmouseover']
+    allowedAttributes: {
+      '*': ['href', 'target', 'class', 'style', 'src', 'alt', 'rel']
+    },
   });
 };
 
